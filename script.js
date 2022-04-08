53575d04e6fe20c60a8c0bcd29cec19f7f2552f0
olivier.ndeye <oliviersndeye@gmail.com>
2022-04-08 10:24:54 -0300

    Update do arquivo javascript, inserindo as funcoes.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let order = [];
-let clickOrder = [];
+let clickedOrder = [];
 let score = 0;
 
 //0 = verde
@@ -13,18 +13,20 @@ const red = document.querySelector('red');
 const green = document.querySelector('green');
 const yellow = document.querySelector('yellow');
 
+
+//criar ordem aleatoria de cores 
 let shuffleOrder = () => {
   let colorOrder = math.floor(math.random() = 4);
-  order[order.lenght] = colorOrder;
-  clickOrder = [];
+  order[order.length] = colorOrder;
+  clickedOrder = [];
 
   for(let i in order){
-  	let elementColor = createElement(order[i]);
+  	let elementColor = createColorElement(order[i]);
   	lightColor(elementColor, Number[i] + 1) 
   }
 }
 
-
+//acende as cores 
 let lightColor = (element, number)=> {
 	time = time + 500;
 	setTimeout(()=> {
@@ -33,4 +35,31 @@ let lightColor = (element, number)=> {
 	setTimeout(() =>{
 		element.classList.remove('selected');
 	},)
+}
+
+
+//checa se a cor selecionada é igual a exibida pela maquina
+let checkOrder = () => {
+	for(let i in clickedOrder){
+		if(clickedOrder[i] != orde[i]){
+			lose();
+			break;
+		}
+	}
+
+	if(clickedOrder.length == order.length){
+		alert(`pontuação: $(score)\n Você Acertou Iniciando próximo nível`)
+		nextLevel();
+	}
+}
+
+let click = (color) => {
+	clickedOrder(clickedOrder.length) = color;
+	createColorElement(color).classList.add('selected');
+
+	setTimeout(() =>{
+		createColorElement(color).classList.add('selected');
+	})
+
+	checkOrder();
 }
